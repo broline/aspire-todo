6c4f33ef16e1284f29f6c54ffb5bf1975186e223
Brian Oline <brian@atlasreality.com>
2024-04-09 11:51:45 -0500

    Fix container port usage

--- src/Todo.AppHost/Program.cs
+++ src/Todo.AppHost/Program.cs
@@ -2,9 +2,8 @@ using Todo.Common;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddSqlServer(Constants.AspireResources.Sql, "MyStrongSqlPassword!", 1433)
+var sql = builder.AddSqlServer(Constants.AspireResources.Sql, "MyStrongSqlPassword!", 53547)
                  .WithVolumeMount("VolumeMount.sqlserver.data", "/var/opt/mssql")
-                 .WithEndpoint(1433, 1433)
                  .AddDatabase(Constants.Database.Name);
 
 var apiService = builder.AddProject<Projects.Todo_Api>(Constants.AspireResources.Api)
